32d6e86bdcf86f46e717a478f400f16d0afc7271
Xukai1666666 <2684024189@qq.com>
2022-09-29 00:17:41 +0800

    全局环境、对象调用中的this

--- this.js
+++ this.js
@@ -17,10 +17,10 @@
 
 
 // 全局环境中的this
-function f1(){
+function f1() {
     console.log(this)
 }
-function f2(){
+function f2() {
     'use strict'
     console.log(this)
 }
@@ -32,16 +32,16 @@ f2()
 // 函数在浏览器全局环境中简单调用
 
 
-const foo={
-    bar:10,
-    fn:function(){
+const foo = {
+    bar: 10,
+    fn: function () {
         console.log(this)
         console.log(this.bar)
     }
 }
- var fn1=foo.fn
- fn1()
- //window
+var fn1 = foo.fn
+fn1()
+//window
 //  undefined
 // 虽然fn函数在foo对象中用来作为对象的方法，但是在赋值给fn1之后
 // fn1仍然是在window的全局环境中执行的。等价于
@@ -49,9 +49,9 @@ const foo={
 // console.log(window.bar)
 
 //上题变种
-const foo={
-    bar:10,
-    fn:function(){
+const foo = {
+    bar: 10,
+    fn: function () {
         console.log(this)
         console.log(this.bar)
     }
@@ -62,3 +62,74 @@ foo.fn()
 // 此时this指向的是最后调用它的对象，在foo.fn()语句中，this指向foo对象
 // 在执行函数时不考虑显式绑定，如果函数中的this是被上一级的对象所调用的，
 // 那么this指向的就是上一级的对象；否则就是全局环境
+
+
+
+结合上下文对象调用的this
+const student = {
+    name: 'lucas'
+    fn: function () {
+        return this
+    }
+}
+console.log(student.fn() == student)
+// true
+
+// 当存在更复杂的调用关系是，如以下代码中的嵌套关系，this会指向最后调用它的对象，因此输出将会是Mike
+
+const person = {
+    name: 'lucas',
+    brother: {
+        name: 'Mike',
+        fn: function () {
+            return this.name
+        }
+    }
+}
+console.log(person.brother.fn()
+)
+// Mike
+
+
+const o1={
+    text:'o1',
+    fn: function () {
+        return this.text
+    }
+}
+const o2={
+    text:'o2',
+    fn: function () {
+        return o1.fn()
+    }
+}
+const o3={
+    text:'o3',
+    fn: function () {
+        var fn=o1.fn
+        return fn()
+    }
+}
+console.log(o1.fn())//我的答案 o1
+console.log(o2.fn())//我的答案 undefined
+console.log(o3.fn())//我的答案 window
+
+//第一个ol不难理解
+//第二个console中的的 o2.fn()最终调用的还是o1.fn(),因此运行结果仍然是o1
+//最后一个console中的o3.fn()通过var fn=o1.fn的赋值进行了“裸奔”调用，因此这里的this指向window，运行结果当然是undefined
+
+const o1={
+    text:'o1',
+    fn: function () {
+        return this.text
+    }
+}
+const o2={
+    text:'o2',
+    fn: o1.fn
+}
+console.log(o2.fn)
+// o2
+
+// this指向最后调用它的对象。
+// 在上面的代码中，提前进行了赋值操作，将函数fn挂载到o2对象上，fn最终作为o2对象的方法被调用。
