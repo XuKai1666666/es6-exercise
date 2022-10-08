6a94d868749e2f6213e9ecd199a0d5ab18efa4d4
Xukai1666666 <2684024189@qq.com>
2022-10-08 20:58:59 +0800

    尾递归

--- function.js
+++ function.js
@@ -93,3 +93,85 @@ function addOne(a) {
 }
 // 上面的函数不会进行尾调用优化，因为内层函数inner用到了外层函数addOne的内部变量one。
 // 注意，目前只有 Safari 浏览器支持尾调用优化，Chrome 和 Firefox 都不支持。
+
+
+
+// 尾递归
+// 递归函数调用自身
+// +尾调用自身  尾递归
+
+// 递归消耗内存，保存很多调用帧，很容易发生栈溢出错误（stack overflow）
+// 对于尾递归来说，由于只存在一个调用帧，所以永远不会发生栈溢出错误。
+
+function factorial(n) {
+    if (n === 1) return1;
+    return n * factorial(n - 1)
+}
+factorial(5)//120
+// 阶乘函数，计算n的阶乘，最多需要保存n个调用记录复杂度O(n)
+
+// 尾递归只保留一个调用记录，复杂度O(1)
+
+function factorial(n, total) {
+    if (n === 1) return total;
+    return factorial(n - 1, n * total);
+}
+factorial(5, 1)
+// 120
+
+
+// 非尾递归 fibonaci数列
+function Fibonacci(n) {
+    if (n <= 1) { return 1 };
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+Fibonacci(10) // 89
+Fibonacci(100) // 超时
+Fibonacci(500) // 超时
+
+//   尾递归优化过的 Fibonacci 数列实现如下。
+function Fibonacci2(n, ac1 = 1, ac2 = 1) {
+    if (n <= 1) { return ac2 };
+
+    return Fibonacci2(n - 1, ac2, ac1 + ac2);
+}
+
+Fibonacci2(100) // 573147844013817200000
+Fibonacci2(1000) // 7.0330367711422765e+208
+Fibonacci2(10000) // Infinity
+// 这就是说，ES6 中只要使用尾递归，就不会发生栈溢出（或者层层递归造成的超时），
+// 相对节省内存。
+
+
+// 蹦床函数并不是真正的尾递归优化，下面的实现才是。
+function tco(f) {
+    var value;
+    var active = false;
+    var accumulated = [];
+
+    return function accumulator() {
+        accumulated.push(arguments);
+        if (!active) {
+            active = true;
+            while (accumulated.length) {
+                value = f.apply(this, accumulated.shift());
+            }
+            active = false;
+            return value;
+        }
+    };
+}
+
+var sum = tco(function (x, y) {
+    if (y > 0) {
+        return sum(x + 1, y - 1)
+    }
+    else {
+        return x
+    }
+});
+
+sum(1, 100000)
+  // 100001
