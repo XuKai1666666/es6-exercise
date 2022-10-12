097381672f0f97d8563ab6444c897612bf3d5647
Xukai1666666 <2684024189@qq.com>
2022-10-12 22:02:29 +0800

    Set（一）

---
+++ set&map.js
@@ -0,0 +1,71 @@
+// Set
+// 基本用法
+// Es6提供了新的数据结构set
+// 类似于数组，但是成员的值都是唯一的，没有重复的值
+
+// Set本身是一个构造函数，用来生成 Set 数据结构。
+const s = new Set();
+
+[2, 3, 5, 4, 5, 2, 2].forEach(x => s.add(x));
+
+for (let i of s) {
+  console.log(i);
+}
+// 2 3 5 4
+
+// add()方法向 Set 结构加入成员，结果表明 Set 结构不会添加重复的值。
+// Set函数可以接受一个数组（或者具有 iterable 接口的其他数据结构）
+// 作为参数，用来初始化。
+
+// 例一
+const set = new Set([1, 2, 3, 4, 4]);
+[...set]
+// [1, 2, 3, 4]
+
+// 例二
+const items = new Set([1, 2, 3, 4, 5, 5, 5, 5]);
+items.size // 5
+
+// 例三
+const set = new Set(document.querySelectorAll('div'));
+set.size // 56
+
+// 类似于
+const set = new Set();
+document
+ .querySelectorAll('div')
+ .forEach(div => set.add(div));
+set.size // 56
+
+// 上面代码也展示了一种去除数组重复成员的方法。
+// 去除数组的重复成员
+[...new Set(array)]
+
+
+// 去除字符串里面的重复字符。
+
+[...new Set('ababbc')].join('')
+// "abc"
+
+// 向 Set 加入值的时候，不会发生类型转换，所以5和"5"是两个不同的值。
+// Set 内部判断两个值是否不同，使用的算法叫做“Same-value-zero equality”，
+// 它类似于精确相等运算符（===），主要的区别是向 Set 加入值时认为NaN等于自身，
+// 而精确相等运算符认为NaN不等于自身。
+
+let set = new Set();
+let a = NaN;
+let b = NaN;
+set.add(a);
+set.add(b);
+set // Set {NaN}
+
+// 上面代码向 Set 实例添加了两次NaN，
+// 但是只会加入一个。这表明，在 Set 内部，两个NaN是相等的。
+
+let set = new Set();
+
+set.add({});
+set.size // 1
+
+set.add({});
+set.size // 2
