55d377d3ccf1e094a5a7a7acbd74450630794a5f
Xukai1666666 <2684024189@qq.com>
2022-10-18 21:08:50 +0800

    apply

--- proxy.js
+++ proxy.js
@@ -443,14 +443,60 @@ proxy.foo // "bar"
 // 注意，set代理应当返回一个布尔值。严格模式下，set代理如果没有返回true，就会报错。
 'use strict';
 const handler = {
-  set: function(obj, prop, value, receiver) {
-    obj[prop] = receiver;
-    // 无论有没有下面这一行，都会报错
-    return false;
-  }
+    set: function (obj, prop, value, receiver) {
+        obj[prop] = receiver;
+        // 无论有没有下面这一行，都会报错
+        return false;
+    }
 };
 const proxy = new Proxy({}, handler);
 proxy.foo = 'bar';
 // TypeError: 'set' on proxy: trap returned falsish for property 'foo'
 
 // 上面代码中，严格模式下，set代理返回false或者undefined，都会报错。
+
+
+
+// apply()
+// apply方法拦截函数的调用、call和apply操作。
+// apply方法可以接受三个参数，分别是目标对象、
+// 目标对象的上下文对象（this）和目标对象的参数数组。
+var handler = {
+    apply(target, ctx, args) {
+        return Reflect.apply(...arguments);
+    }
+};
+
+var target = function () { return 'I am the target'; };
+var handler = {
+    apply: function () {
+        return 'I am the proxy';
+    }
+};
+
+var p = new Proxy(target, handler);
+
+p()
+// "I am the proxy"
+
+// 上面代码中，变量p是 Proxy 的实例，
+// 当它作为函数调用时（p()），就会被apply方法拦截，返回一个字符串。
+
+
+var twice = {
+    apply(target, ctx, args) {
+        return Reflect.apply(...arguments) * 2;
+    }
+};
+function sum(left, right) {
+    return left + right;
+};
+var proxy = new Proxy(sum, twice);
+proxy(1, 2) // 6
+proxy.call(null, 5, 6) // 22
+proxy.apply(null, [7, 8]) // 30
+
+// 上面代码中，每当执行proxy函数（直接调用或call和apply调用），就会被apply方法拦截。
+
+// 直接调用Reflect.apply方法，也会被拦截。
+Reflect.apply(proxy, null, [9, 10]) // 38
