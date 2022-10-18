9062747aaeafaa59dd9aaf1929265173344c0ddf
Xukai1666666 <2684024189@qq.com>
2022-10-18 21:27:01 +0800

    deleteProperty

--- proxy.js
+++ proxy.js
@@ -648,3 +648,30 @@ const handler = {
 
 let p = new Proxy(function () { }, handler);
 new p() // true
+
+
+// deleteProperty()
+// deleteProperty方法用于拦截delete操作，
+// 如果这个方法抛出错误或者返回false，当前属性就无法被delete命令删除。
+var handler = {
+    deleteProperty(target, key) {
+        invariant(key, 'delete');
+        delete target[key];
+        return true;
+    }
+};
+function invariant(key, action) {
+    if (key[0] === '_') {
+        throw new Error(`Invalid attempt to ${action} private "${key}" property`);
+    }
+}
+
+var target = { _prop: 'foo' };
+var proxy = new Proxy(target, handler);
+delete proxy._prop
+  // Error: Invalid attempt to delete private "_prop" property
+
+//   deleteProperty方法拦截了delete操作符，删除第一个字符为下划线的属性会报错。
+
+
+// 目标对象自身的不可配置（configurable）的属性，不能被deleteProperty方法删除，否则报错。
