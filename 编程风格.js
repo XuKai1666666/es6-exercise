2619b7976c7eaba98249ed7d7f6fd8c21717c46e
Xukai1666666 <2684024189@qq.com>
2022-11-14 20:58:06 +0800

    map

--- 编程风格.js
+++ 编程风格.js
@@ -296,3 +296,22 @@ function handleThings(opts) {
 function handleThings(opts = {}) {
   // ...
 }
+
+// Map 结构
+// 注意区分 Object 和 Map，
+// 只有模拟现实世界的实体对象时，才使用 Object。
+// 如果只是需要key: value的数据结构，使用 Map 结构。因为 Map 有内建的遍历机制。
+
+let map = new Map(arr);
+
+for (let key of map.keys()) {
+  console.log(key);
+}
+
+for (let value of map.values()) {
+  console.log(value);
+}
+
+for (let item of map.entries()) {
+  console.log(item[0], item[1]);
+}
